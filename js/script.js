fb641b4dc49fe6b52d3dea211553fc69f982a32c
Angie <77899962+aciturbide@users.noreply.github.com>
2021-04-12 13:14:41 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 function init(){
 //add your javascript between these two lines of code
-function listenerText(){
-var text = document.getElementById("entryinput");
-var displayoutputtext = document.getElementById("textoutput");
-  alert("Angie Iturbide: " + text.value);
-  textoutput.innerHTML = text.value;
+  function listenerandtext(){
+    var text = document.getElementById("entryinput");
+    var displayoutputtext = document.getElementById("textoutput");
+    alert("Angie Iturbide: " + text.value);
+    displayoutputtext.innerHTML = text.value;
 }
-var textbox= document.getElementById("entrybutton");
-textbox.addEventListener('click', listenerText);
+
+var textbox = document.getElementById("entrybutton");
+
+textbox.addEventListener('click', myFunction);
+
 }
+
 window.addEventListener('load', init);
