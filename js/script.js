67ebb33304e72de687d04bf34c5aac8124a65091
Angie <77899962+aciturbide@users.noreply.github.com>
2021-04-12 13:07:36 -0500

    switched order of calling code

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 function init(){
-//add your javascrip between these two lines of code
-var input = document.getElementById("entrybutton");
-  function listenerText() {
-    var text = document.getElementById("entryinput");
-    var displayoutputtext = document.getElementById("textoutput");
-    alert("Angie Iturbide: " + text.value);
-    output.innerHTML = text.value;
-  }
-   entry.addEventListener('click', listenerText);
+//add your javascript between these two lines of code
+function listenerText(){
+var text = document.getElementById("entryinput");
+var displayoutputtext = document.getElementById("textoutput");
+  alert("Angie Iturbide: " + text.value);
+  output.innerHTML = text.value;
+}
+
+var textbox= document.getElementById("entrybutton");
+
+textbox.addEventListener('click', listenerText);
+
 }
 
 window.addEventListener('load', init);
