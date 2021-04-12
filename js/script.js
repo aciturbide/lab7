38e0738b9108501749b477f8d90674a61378164a
Angie <77899962+aciturbide@users.noreply.github.com>
2021-04-12 12:56:38 -0500

    added js code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
-
-
-
-
-
-
-
+var input = document.getElementById("entrybutton");
+  function listenerText() {
+    var text = document.getElementById("entryinput");
+    var displayoutputtext = document.getElementById("textoutput");
+    alert("Angie Iturbide: " + text.value);
+    output.innerHTML = text.value;
+  }
+   entry.addEventListener('click', listenerText);
+}
 
 window.addEventListener('load', init);
